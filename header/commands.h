1cd2b165b4d2e947e84a44a9ae72ea45dc0e165f
chrry03 <ocr925@dgu.ac.kr>
2026-05-22 21:51:20 +0900

    feat: 추가 명령어 3개(아직미정) commands.h에 추가하고, 소스코드파일 생성

--- header/commands.h
+++ header/commands.h
@@ -3,10 +3,8 @@
 
 #include "filesystem.h"
 
-/* 현재 경로 출력 */
-void command_pwd(FileSystem* fs);
-
 /* 공통 필수 명령어 */
+void command_pwd(FileSystem* fs);
 void command_ls(FileSystem* fs, int argc, char* argv[]);
 void command_cd(FileSystem* fs, int argc, char* argv[]);
 void command_mkdir(FileSystem* fs, int argc, char* argv[]);
@@ -17,5 +15,10 @@ void command_grep(FileSystem* fs, int argc, char* argv[]);
 void command_chown(FileSystem* fs, int argc, char* argv[]);
 void command_mv(FileSystem* fs, int argc, char* argv[]);
 void command_rm(FileSystem* fs, int argc, char* argv[]);
+
+/* 추가 구현 명령어 */
+void command_touch(FileSystem* fs, int argc, char* argv[]);
+void command_tree(FileSystem* fs, int argc, char* argv[]);
+void command_find(FileSystem* fs, int argc, char* argv[]);
 
 #endif
